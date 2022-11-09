250f02fc0e9fe9dc8b05fb7fcc0defd9e1219519
Marcelo Mariano <marcelomariano11@gmail.com>
2022-11-09 09:51:51 -0500

    add somamultiplos file and method

--- somamultiplos.js
+++ somamultiplos.js
@@ -1,10 +1,11 @@
-
-
-let numero = 10
-
-for (i = numero - 1; i > 0; i--) {
-
-
-
-
+function multiplos(numero) {
+    let soma = 0
+    for (i = numero - 1; i > 0; i--) {
+        if (i % 5 == 0 || i % 3 == 0) {
+            soma += i
+        }
+    }
+    return soma
 }
+
+console.log(multiplos(15))
